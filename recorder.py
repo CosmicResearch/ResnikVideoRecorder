8fd35ed7528837c2805d889bc199baa6e2579676
Mkaruso <guillem.castro@hotmail.com>
2017-01-31 21:18:51 +0100

    Added camera script

---
+++ recorder.py
@@ -0,0 +1,9 @@
+import picamera
+
+camera = picamera.PiCamera(resolution=(1640, 1232), framerate=40)
+camera.start_recording('1.h264')
+camera.wait_recording(30)
+for i in range(2, 100):
+    camera.split_recording('%d.h264' % i)
+    camera.wait_recording(30)
+camera.stop_recording()
